acf30643416b664bf21965d907f836a734021140
Nathandiv <k.n.hlungwani@gmail.com>
2024-08-16 11:11:37 +0200

    added functionalty

--- main.js
+++ main.js
@@ -17,3 +17,39 @@ con.connect((err)=>{
     }
 }) 
 
+
+app.post ('/', (req, res)=>{
+res.send('Home')
+
+})
+
+
+app.post('/post',(req,res)=>{
+    const id=req.body.id;
+    const FirstName=req.body.FirstName;
+    const LastName=req.body.FirstName;
+    const ArtSkill=req.body.ArtSkill;
+    const COUNTRY=req.body.COUNTRY;
+    const Age=req.body.Age;
+    const Email=req.body.Email;
+    const Cellphone=req.body.Cellphone;
+
+    con.query('insert into ArtistName values(?,?,?,?,?,?,?',[id,FirstName,LastName,ArtSkill,COUNTRY,Age,Email,Cellphone],(err,result)=>{
+
+        if(err){
+            console.log(err)
+        }else {
+            res.send("POSTED")
+        }
+
+    })
+})
+
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err)
+    }else {
+        console.log("on port 3000 !!")
+    }
+
+});
